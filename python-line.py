35d5dbcefcfc53fb495d5e8bb86bff31606c99f7
JPNET <43113652+jpnetwork@users.noreply.github.com>
2021-01-04 20:26:58 +0700

    Create python-line.py

---
+++ python-line.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python
+import requests
+
+url = 'https://notify-api.line.me/api/notify'
+token = 'XXXXXXXXXXXXXXXXXXXX' # Line Notify Token
+img = {'imageFile': open('picture.jpg','rb')}
+data = {'message':'Test from python3'}
+headers = {'Authorization':'Bearer ' + token}
+session = requests.Session()
+session_post = session.post(url, headers=headers, files=img, data =data)
+print(session_post.text) 
